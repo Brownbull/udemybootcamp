671c2639a785b24af9b8ba7d37448144a7669a3d
brownbull <carcamo.gabriel@gmail.com>
2017-11-29 11:28:58 -0300

    npm intall

---
+++ NodeJS/Basics.sh
@@ -0,0 +1,10 @@
+# How to use npm install
+$ cd "path/to/app/deirectory"
+# Initializze
+$ npm init
+# setup app
+$ npm install "app name"
+
+# How to use require()
+var cat = require("cat-me");
+console.log(cat());
