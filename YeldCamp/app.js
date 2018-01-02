034a36a5863937e655662b7fbeec36d6b25de259
Brownbull <carcamo.gabriel@gmail.com>
2018-01-02 19:49:31 -0300

    Comments base done

--- YeldCamp/app.js
+++ YeldCamp/app.js
@@ -13,7 +13,7 @@ mongoose.connect("mongodb://localhost/yelp_camp2");
 seedDB();
 
 // Setup public directory
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 // INDEX
