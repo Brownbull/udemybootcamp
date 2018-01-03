47d2d4e5bcc289bfe8d7a294c1e1cb62bac3baea
brownbull <carcamo.gabriel@gmail.com>
2018-01-03 09:32:08 -0300

    modified models to first letter uppercase

--- YeldCamp/app.js
+++ YeldCamp/app.js
@@ -1,21 +1,39 @@
-var express     = require("express");
-var app         = express();
-var request     = require("request");
-var bodyParser  = require("body-parser");
-var mongoose    = require("mongoose");
-var seedDB      = require("./seeds");
-//Models
-var camp        = require("./models/campground");
-var comment     = require("./models/comment");
+// LIBS
+var express               = require("express");
+var app                   = express();
+var request               = require("request");
+var bodyParser            = require("body-parser");
+var mongoose              = require("mongoose");
+var seedDB                = require("./seeds");
+var passport              = require("passport");
+var bodyParser            = require("body-parser");
+var localStrategy         = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
 
-// DB setup
+// MODELS
+var Camp        = require("./models/campground");
+var Comment     = require("./models/comment");
+var User        = require('./models/user');
+
+// SETUP
 mongoose.connect("mongodb://localhost/yelp_camp2");
 seedDB();
-
-// Setup public directory
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+  secret: "Tommy is the mas pulentio",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser()); // code session info
+passport.deserializeUser(User.deserializeUser()); // decode session info
 
+//=============
+// ROUTES
+//=============
 // INDEX
 app.get("/", function(req, res){
   res.render("landing.ejs");
@@ -24,7 +42,7 @@ app.get("/", function(req, res){
 // INDEX
 app.get("/campgrounds", function(req, res){
   // Get all campgrounds from DB
-  camp.find({}, function(err, camps){
+  Camp.find({}, function(err, camps){
     if (err) {
       console.log(err);
     } else {
@@ -42,14 +60,14 @@ app.get("/campgrounds/new", function(req, res){
 // SHOW
 app.get("/campgrounds/:id", function(req, res){
   // find camp with id
-  camp.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+  Camp.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
     if (err) {
       console.log(err);
     } else {
       // console.log(foundCampground);
       res.render("campgrounds/show.ejs", {campground: foundCampground});
-    } //eof if/else of camp.findById
-  }) // eof camp.findById
+    } //eof if/else of Camp.findById
+  }) // eof Camp.findById
 }); // eof app.get
 
 // CREATE
@@ -61,7 +79,7 @@ app.post("/campgrounds", function(req, res){
   var newCampground = {name: name, image: image, description: description};
   // campgrounds.push(newCampground);
   // Crate new camp on DB
-  camp.create(newCampground, function(err, newcamp){
+  Camp.create(newCampground, function(err, newcamp){
     if (err) {
       console.log(err);
     } else {
@@ -75,33 +93,33 @@ app.post("/campgrounds", function(req, res){
 // ===========================
 // New     /dogs/new          GET
 app.get("/campgrounds/:id/comments/new", function(req, res){
-  camp.findById(req.params.id, function(err, campData){
+  Camp.findById(req.params.id, function(err, campData){
     if (err) {
       console.log(err);
     } else {
       res.render("comments/new.ejs", {campground: campData});
-    } // eof if/else of camp.findById
-  }) // eof camp.findById
+    } // eof if/else of Camp.findById
+  }) // eof Camp.findById
 }); //eof app.get
 
 // Create  /dogs     ->Rdrct  POST    Dog.create()
 app.post("/campgrounds/:id/comments", function(req, res){
-  camp.findById(req.params.id, function(err, campData){
+  Camp.findById(req.params.id, function(err, campData){
     if (err) {
       console.log(err);
       res.redirect("/campgrounds");
     } else {
-      comment.create(req.body.comment, function(err, comment){
+      Comment.create(req.body.comment, function(err, comment){
         if (err) {
           console.log(err);
         } else {
           campData.comments.push(comment);
           campData.save();
           res.redirect('/campgrounds/' + campData._id)
-        } // eof if/else of comment.create
-      }); // eof comment.create
-    } // eof if/else of camp.findById
-  }); // eof camp.findById
+        } // eof if/else of Comment.create
+      }); // eof Comment.create
+    } // eof if/else of Camp.findById
+  }); // eof Camp.findById
 }); //eof app.post
 
 // LISTEN - start
