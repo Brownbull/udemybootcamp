8a794d1543206af26409549b28a809509807c214
Brownbull <carcamo.gabriel@gmail.com>
2018-01-06 01:00:08 -0300

    deploy app done

---
+++ DeployDemo/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.render("home.ejs")
+});
+
+app.get("/about", function(req, res){
+res.render("about.ejs")
+});
+
+// LISTEN - start
+// app.listen(process.env.PORT, process.env.IP, function (req, res) {
+app.listen(3000, 'localhost', function (req, res) {
+    console.log("Server Started!");
+});
+// LISTEN - end
